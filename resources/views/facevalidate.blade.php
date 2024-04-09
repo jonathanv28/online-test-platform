d23253e86392034646889366c2147c4cd3ff6b99
Jonathan Victor <jonathanaibaho@gmail.com>
2024-04-09 13:36:23 +0700

    update

--- resources/views/facevalidate.blade.php
+++ resources/views/facevalidate.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto pt-16">
-        <h1 class="text-center font-bold text-4xl">Hi, Jonathan Victor 👋</h1>
+        <h1 class="text-center font-bold text-4xl mb-8">Validasi Wajah</h1>
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -21,13 +21,13 @@
         {{-- <input
             class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
             id="photo" type="file" name="photo1"> --}}
-        <div id="webcam-container">
+        <div id="webcam-container" class="mb-6">
             <!-- Webcam preview will be shown here -->
         </div>
         <input type="hidden" name="image" id="captured-image">
         <div class="form-group">
             <button type="submit"
-            class="w-full text-white bg-grass hover:bg-grassbold focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Daftar</button>
+            class="w-full text-white bg-grass hover:bg-grassbold focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Ambil & Validasi Foto</button>
         </div>
     </form>
     @endif
@@ -37,8 +37,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 <script language="JavaScript">
     Webcam.set({
-        width: 320,
-        height: 240,
+        width: 640,
+        height: 360,
         image_format: 'jpeg',
         jpeg_quality: 90
     });
